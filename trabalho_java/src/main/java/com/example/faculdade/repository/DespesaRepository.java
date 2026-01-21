3e7e4c06279b55327d2583852b388f22342ae50b
Mateus Nunes <jmateus12326609@gmail.com>
2026-01-21 08:26:12 -0300

    Atualizar o DespesaRepository.java

--- trabalho_java/src/main/java/com/example/faculdade/repository/DespesaRepository.java
+++ trabalho_java/src/main/java/com/example/faculdade/repository/DespesaRepository.java
@@ -4,8 +4,7 @@ package com.example.faculdade.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.faculdade.model.Despesa;
 
-// A anotação @Repository é opcional aqui, mas é uma boa prática.
-// O Spring já entende que é um repositório porque estende JpaRepository.
+
 public interface DespesaRepository extends JpaRepository<Despesa, Long> {
 
 }
